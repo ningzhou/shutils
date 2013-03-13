e4a0a98beb5cc56967b0deb1d76b0f4d0c452226
ningzhou <ningzhou.cn@gmail.com>
2013-03-13 16:57:25 -0400

    change the tmux installed path from $HOME/local to $HOME/.local

--- tmux/tmux_local_install.sh
+++ tmux/tmux_local_install.sh
@@ -8,7 +8,7 @@
 set -e
  
 # create our directories
-mkdir -p $HOME/local $HOME/tmux_tmp
+mkdir -p $HOME/.local $HOME/tmux_tmp
 cd $HOME/tmux_tmp
  
 # download source files for tmux, libevent, and ncurses
@@ -23,7 +23,7 @@ wget ftp://ftp.gnu.org/gnu/ncurses/ncurses-5.9.tar.gz
 ############
 tar xvzf libevent-2.0.19-stable.tar.gz
 cd libevent-2.0.19-stable
-./configure --prefix=$HOME/local --disable-shared
+./configure --prefix=$HOME/.local --disable-shared
 make
 make install
 cd ..
@@ -33,7 +33,7 @@ cd ..
 ############
 tar xvzf ncurses-5.9.tar.gz
 cd ncurses-5.9
-./configure --prefix=$HOME/local
+./configure --prefix=$HOME/.local
 make
 make install
 cd ..
@@ -43,12 +43,12 @@ cd ..
 ############
 tar xvzf tmux-1.6.tar.gz
 cd tmux-1.6
-./configure CFLAGS="-I$HOME/local/include -I$HOME/local/include/ncurses" LDFLAGS="-L$HOME/local/lib -L$HOME/local/include/ncurses -L$HOME/local/include"
-CPPFLAGS="-I$HOME/local/include -I$HOME/local/include/ncurses" LDFLAGS="-static -L$HOME/local/include -L$HOME/local/include/ncurses -L$HOME/local/lib" make
-cp tmux $HOME/local/bin
+./configure CFLAGS="-I$HOME/.local/include -I$HOME/.local/include/ncurses" LDFLAGS="-L$HOME/.local/lib -L$HOME/.local/include/ncurses -L$HOME/.local/include"
+CPPFLAGS="-I$HOME/.local/include -I$HOME/.local/include/ncurses" LDFLAGS="-static -L$HOME/.local/include -L$HOME/.local/include/ncurses -L$HOME/.local/lib" make
+cp tmux $HOME/.local/bin
 cd ..
  
 # cleanup
 rm -rf $HOME/tmux_tmp
  
-echo "$HOME/local/bin/tmux is now available. You can optionally add $HOME/local/bin to your PATH."
+echo "$HOME/.local/bin/tmux is now available. You can optionally add $HOME/.local/bin to your PATH."
